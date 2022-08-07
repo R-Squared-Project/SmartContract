459e282cba81bcc04a1c0853692be03eacb21c93
walteratwork <72606171+walteratwork@users.noreply.github.com>
2022-08-08 01:07:29 +0400

    added ganache

--- truffle-config.js
+++ truffle-config.js
@@ -41,11 +41,12 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
+    development: {
     //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+      host: "ganache",
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
